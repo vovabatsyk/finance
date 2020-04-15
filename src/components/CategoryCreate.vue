147d788410116f9c9fc547648200d0c28ae43b6a
Vova Batsyk <vovabatsyk@gmail.com>
2020-04-15 12:08:10 +0300

    edit category

--- src/components/CategoryCreate.vue
+++ src/components/CategoryCreate.vue
@@ -7,29 +7,29 @@
       <form @submit.prevent="submitHandler">
         <div class="input-field">
           <input
-                  id="name"
-                  type="text"
-                  v-model="title"
-                  :class="{invalid: $v.title.$dirty && !$v.title.required}"
-          >
+            id="name"
+            type="text"
+            v-model="title"
+            :class="{ invalid: $v.title.$dirty && !$v.title.required }"
+          />
           <label for="name">Название</label>
           <span
-                  v-if="$v.title.$dirty && !$v.title.required"
-                  class="helper-text invalid"
+            v-if="$v.title.$dirty && !$v.title.required"
+            class="helper-text invalid"
           >Введите название</span>
         </div>
         <div class="input-field">
           <input
-                  id="limit"
-                  type="number"
-                  v-model.number="limit"
-                  :class="{invalid: $v.limit.$dirty && !$v.limit.minValue}"
-          >
+            id="limit"
+            type="number"
+            v-model.number="limit"
+            :class="{ invalid: $v.limit.$dirty && !$v.limit.minValue }"
+          />
           <label for="limit">Лимит</label>
           <span
-                  v-if="$v.limit.$dirty && !$v.limit.minValue"
-                  class="helper-text invalid"
-          >Минимальная величина {{$v.limit.$params.minValue.min}}</span>
+            v-if="$v.limit.$dirty && !$v.limit.minValue"
+            class="helper-text invalid"
+          >Минимальная величина {{ $v.limit.$params.minValue.min }}</span>
         </div>
 
         <button class="btn waves-effect waves-light" type="submit">
@@ -42,41 +42,39 @@
 </template>
 
 <script>
-    import {required, minValue} from 'vuelidate/lib/validators'
+import { required, minValue } from "vuelidate/lib/validators";
 
-    export default {
-        data: () => ({
-            title: '',
-            limit: 100
-        }),
-        validations: {
-            title: {required},
-            limit: {minValue: minValue(100)}
-        },
-        mounted() {
-            M.updateTextFields()
-        },
-        methods: {
-            async submitHandler() {
-                if (this.$v.$invalid) {
-                    this.$v.$touch()
-                    return
-                }
-                try {
-                    const category = await this.$store.dispatch('createCategory', {
-                        title: this.title,
-                        limit: this.limit
-                    })
-                    this.$emit('created', category)
+export default {
+  data: () => ({
+    title: "",
+    limit: 100
+  }),
+  validations: {
+    title: { required },
+    limit: { minValue: minValue(100) }
+  },
+  mounted() {
+    M.updateTextFields();
+  },
+  methods: {
+    async submitHandler() {
+      if (this.$v.$invalid) {
+        this.$v.$touch();
+        return;
+      }
+      try {
+        const category = await this.$store.dispatch("createCategory", {
+          title: this.title,
+          limit: this.limit
+        });
+        this.$emit("created", category);
 
-                    this.$message(`Категория \"${category.title}\" создана успешно!`)
-                    this.title = ''
-                    this.limit = 100
-                    this.$v.reset()
-                } catch (e) {
-                }
-
-            }
-        }
+        this.$message(`Категория \"${category.title}\" создана успешно!`);
+        this.title = "";
+        this.limit = 100;
+        this.$v.reset();
+      } catch (e) {}
     }
+  }
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -1,46 +1,44 @@
-import Vue from 'vue'
-import Vuelidate from 'vuelidate'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import dateFilter from '@/filter/date.filter'
-import currencyFilter from '@/filter/currency.filter'
-import messagePlugin from '@/utils/message.plugin'
-import Loader from '@/components/app/Loader'
-import './registerServiceWorker'
-import 'materialize-css/dist/js/materialize.min'
+import Vue from "vue"
+import Vuelidate from "vuelidate"
+import App from "./App.vue"
+import router from "./router"
+import store from "./store"
+import dateFilter from "@/filter/date.filter"
+import currencyFilter from "@/filter/currency.filter"
+import messagePlugin from "@/utils/message.plugin"
+import Loader from "@/components/app/Loader"
+import "./registerServiceWorker"
+import "materialize-css/dist/js/materialize.min"
 
-import firebase from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/database'
+import firebase from "firebase/app"
+import "firebase/auth"
+import "firebase/database"
 
 Vue.use(messagePlugin)
 Vue.config.productionTip = false
-Vue.filter('date', dateFilter)
-Vue.filter('currency', currencyFilter)
+Vue.filter("date", dateFilter)
+Vue.filter("currency", currencyFilter)
 Vue.use(Vuelidate)
-Vue.component('Loader', Loader)
+Vue.component("Loader", Loader)
 
 // Initialize Firebase
 firebase.initializeApp({
-    apiKey: 'AIzaSyDO80oeGtic2i_eWbmBUMn2CqrdBSxrgvA',
-    authDomain: 'vue-finance-34d79.firebaseapp.com',
-    databaseURL: 'https://vue-finance-34d79.firebaseio.com',
-    projectId: 'vue-finance-34d79',
-    storageBucket: 'vue-finance-34d79.appspot.com',
-    messagingSenderId: '189392031748',
-    appId: '1:189392031748:web:394e9501cbc04b8a6ea551',
-    measurementId: 'G-66XJVQLBVR'
+  apiKey: "AIzaSyDO80oeGtic2i_eWbmBUMn2CqrdBSxrgvA",
+  authDomain: "vue-finance-34d79.firebaseapp.com",
+  databaseURL: "https://vue-finance-34d79.firebaseio.com",
+  projectId: "vue-finance-34d79",
+  storageBucket: "vue-finance-34d79.appspot.com",
+  messagingSenderId: "189392031748",
+  appId: "1:189392031748:web:394e9501cbc04b8a6ea551",
+  measurementId: "G-66XJVQLBVR"
 })
 let app
 firebase.auth().onAuthStateChanged(() => {
-    if (!app) {
-        app = new Vue({
-            router,
-            store,
-            render: h => h(App)
-        }).$mount('#app')
-    }
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount("#app")
+  }
 })
-
-
